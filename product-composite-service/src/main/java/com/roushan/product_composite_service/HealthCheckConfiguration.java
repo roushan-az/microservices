f289c52e2289cc95306967244003007e87d387f0
roushan <roushan.astrazeneca@gmail.com>
2025-07-20 20:52:15 +0530

    Initial commit - Spring Boot Microservices project

---
+++ product-composite-service/src/main/java/com/roushan/product_composite_service/HealthCheckConfiguration.java
@@ -0,0 +1,31 @@
+package com.roushan.product_composite_service;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.roushan.product_composite_service.services.ProductCompositeIntegration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.CompositeReactiveHealthContributor;
+import org.springframework.boot.actuate.health.ReactiveHealthContributor;
+import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HealthCheckConfiguration {
+
+  @Autowired
+  ProductCompositeIntegration integration;
+
+  @Bean
+  ReactiveHealthContributor coreServices() {
+
+    final Map<String, ReactiveHealthIndicator> registry = new LinkedHashMap<>();
+
+    registry.put("product", () -> integration.getProductHealth());
+    registry.put("recommendation", () -> integration.getRecommendationHealth());
+    registry.put("review", () -> integration.getReviewHealth());
+
+    return CompositeReactiveHealthContributor.fromMap(registry);
+  }
+}
